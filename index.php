bdae8f5a84b3d7466f22ad47a733ad4f10cbafb9
TouradGithub <95051771+TouradGithub@users.noreply.github.com>
2022-12-22 12:03:53 +0100

    remove bootstrap
    remove bootstrap

--- index.php
+++ index.php
@@ -99,7 +99,6 @@ if (isset($_POST['creer']) && isset($_POST['nom'])){
 <head>
 	<link rel="stylesheet" media="screen" type="text/css" title="Design" href="theme.css" />
 <h1>   <marquee> projet php fromwork </marquee> </h1>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <div id="box">
 <h2><u>Nombre de Etudiant créés : <?= $Etudiants->count() ?></u></h2>
 
@@ -179,7 +178,6 @@ if (isset($_POST['creer']) && isset($_POST['nom'])){
      
      
     </body>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </html>
 <?php
 if (isset($Etud)){
